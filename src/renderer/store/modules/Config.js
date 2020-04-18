999bf62501e38c339b403f3552847db34f78aa3d
kitekii <tim720719@163.com>
2020-04-18 17:44:19 +0800

    数据结构

---
+++ src/renderer/store/modules/Config.js
@@ -0,0 +1,19 @@
+const state = {
+  title: 'Go邦邦',
+  showSteps: true,
+  first: 1
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
--- src/renderer/store/modules/Map.js
+++ src/renderer/store/modules/Map.js
@@ -1,9 +1,9 @@
 import {
   SET_BOARD,
   SET_STEPS,
-  RESET_BOARD,
-  BACKWARD,
-  FORWARS
+  RESET_BOARD
+  // BACKWARD,
+  // FORWARS
 } from '../mutations.js'
 
 const emptyBoard = () => {
@@ -26,18 +26,31 @@ const emptyBoard = () => {
   ]
 }
 
-const copyBoard = a => a.map(r => r.slice()).slice()
+// const copyBoard = a => a.map(r => r.slice()).slice()
 
 const state = {
-  board: emptyBoard()
+  board: emptyBoard(),
+  steps: [
+    /*
+    {
+        position: [7,7],
+        role: 1
+    }
+    */
+  ],
+  fives: []
 }
 
 const mutations = {
-  DECREMENT_MAIN_COUNTER (state) {
-    state.main--
+  [RESET_BOARD] (state) {
+    state.board = emptyBoard()
+    state.step = []
   },
-  INCREMENT_MAIN_COUNTER (state) {
-    state.main++
+  [SET_BOARD] (state, board) {
+    state.board = board
+  },
+  [SET_STEPS] (state, steps) {
+    state.steps = steps
   }
 }
 
