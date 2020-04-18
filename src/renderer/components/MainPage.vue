1bdc177b34a88f9bd36fcc7467c0245e4a178b7e
kitekii <tim720719@163.com>
2020-04-18 16:00:38 +0800

    redirection

---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="wrapper">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main-page',
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  
+</style>
