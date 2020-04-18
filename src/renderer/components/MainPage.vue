e4e9b3637d135b13c29f6b3ee2933c1fa85afa7b
kitekii <tim720719@163.com>
2020-04-18 17:45:27 +0800

    棋盘界面

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,18 +1,26 @@
 <template>
-  <div id="wrapper">
-    
+  <div id="home">
+    <h1 class="title">{{config.title}}</h1>
+    <Board @set='set'></Board>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'main-page',
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      }
+import Board from './sub/Board'
+export default {
+  name: 'main-page',
+  components: {Board},
+  computed: {
+    config () {
+      return this.$store.state.Config
+    }
+  },
+  methods: {
+    open (link) {
+      this.$electron.shell.openExternal(link)
     }
   }
+}
 </script>
 
 <style>
