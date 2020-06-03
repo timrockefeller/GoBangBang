8fccf5fd2a52b5cc79d3827d40e8597b90d94bf3
Zanbuffet <zanbuffet@126.com>
2020-06-04 03:45:49 +0800

    搞到了一份代码，看了看结构，瞎改瞎copy了一些
    https://github.com/lihongxun945/gobang

--- src/renderer/utils/ailib/method.js
+++ src/renderer/utils/ailib/method.js
@@ -6,12 +6,63 @@
  */
 //options为树枝深度deep
 //负极大值算法-极大极小值的简化版，双方均力求分数极大值
-
-var negamax = function(candidates, role, deep, alpha, beta){
-  return alpha
-}
 import * as R from './roles.js'
 import * as S from './score.js'
+
+var negamax = function(board,role,deep,alpha,beta){
+  var best = 0;
+  var point=gen(board,deep)
+  for(var i=0; i<point.length; i++){
+    var p=point[i]
+    board.put(p,role)
+    var steps=[p]
+    var v= r(deep-1,-beta,-alpha,R.reverse(role),1,steps.slice(0),0)//迭代,r是包含了剪枝的寻找best_score的算法，跟这个类似
+    v.score *=-1//负极大值算法精髓
+    alpha = Math.max(alpha,v.score)
+    board.remove(p)
+    p.v = v
+  return alpha
+}
+//生成可以下棋子的位置列表
+//因为不需要遍历棋盘所有位置，这个函数主要是把搜索范围放在有棋子的周围一两格范围内
+var gen = function(board,deep){
+  var neighbors = [];
+  var nextNeighbors = [];
+
+  for(var i=0;i<board.length;i++) {
+    for(var j=0;j<board[i].length;j++) {
+      if(board[i][j] == R.EMPTY) {
+        if(hasNeighbor(board, [i, j], 1, 1)) { //必须是有邻居的才行
+            neighbors.push([i, j]);
+        } else if(deep >= 2 && hasNeighbor(board, [i, j], 2, 2)) {
+          nextNeighbors.push([i, j]);
+        }
+      }
+    }
+  }
+  return  neighbors.concat(nextNeighbors)//返回可以下棋子的列表
+}
+//检测一个棋子周围是否有邻居
+var hasNeighbor = function(board,[x,y], distance, count) {
+  var len = board.length
+  var startX = x-distance
+  var endX = x+distance
+  var startY = y-distance
+  var endY = y+distance
+  for(var i=startX;i<=endX;i++) {
+    if(i<0||i>=len) continue
+    for(var j=startY;j<=endY;j++) {
+      if(j<0||j>=len) continue
+      if(i==x && j==y) continue
+      if(board[i][j] != R.empty) {
+        count --
+        if(count <= 0) return true
+      }
+    }
+  }
+  return false
+}
+
 var evaluate = function (role) {//获取当前操作对象
   let comMaxScore=0
   let humMaxScore=0
@@ -48,7 +99,11 @@ var GainScore = function (type){
 }
 
 const next = function (board, options = {}) {
-  return [0, 0]
+  var p=[0,0]
+  //board.push(p,role) 等一个有缘人来补充board类，p是位置
+  //return p
+  //if (count <=0 ) p=[7,7] return p  要是电脑先起手的话直接下在7,7的位置即可
+  return p
 }
 
 export default {
